5de6327797efce9886f73ead30f935de1b9b586c
aaron <aaron@bolcerek.com>
2026-07-15 19:05:36 -0400

    Added chapters 9 and 11

---
+++ c11/add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+auto add(auto x, auto y) {
+    return x + y;
+}
+
+int main() {
+    std::cout << add(2, 3) << '\n';
+    std::cout << add(2.3, 3.4) << '\n';
+
+    return 0;
+}
---
+++ c11/mult.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+auto mult(auto x, auto y) {
+    return x * y;
+}
+
+int main()
+{
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+
+	return 0;
+}
